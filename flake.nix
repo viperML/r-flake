378436de3a2c217d4be78f79876cf603aab85e80
Fernando Ayats <ayatsfer@gmail.com>
2023-05-28 20:55:54 +0000

    no aarch

--- flake.nix
+++ flake.nix
@@ -10,7 +10,7 @@
     ...
   }:
     flake-parts.lib.mkFlake {inherit inputs;} {
-      systems = ["x86_64-linux" "aarch64-linux"];
+      systems = ["x86_64-linux"];
 
       perSystem = {pkgs, ...}: let
         nv = pkgs.callPackages ./generated.nix {};
